b15c0a884fdd454c7d609586422d4ed1f2a0e419
Emmanuel Valdez <evaldez89@gmail.com>
2019-03-25 21:38:37 -0400

    use schema and Open API for documentation

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,14 +1,17 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from snippets import views
+from rest_framework.schemas import get_schema_view
 
+
+schema_view = get_schema_view(title='Pastebin API')
 
 router = DefaultRouter()
-# router.register(r'', views.api_root)
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
 
 urlpatterns = [
+     path('schema/', schema_view),
     path('', include(router.urls)),
 ]
